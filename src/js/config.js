1279509b2e3970cddd6123150a71532c47ab7ac8
Pragathesh-1111 <pragatheshwaran2006@gmail.com>
2026-06-14 19:48:35 +0530

    new welcome feature

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,8 @@ export const TIMEOUT_SEC = 10;
 export const RES_PAGE = 15;
 export const KEY = '9064dfa7-d073-4508-80af-a1bb8419f9e0';
 export const MODEL_CLOSE_SEC = 2.5;
+
+const newFeature = function() {
+    console.log('welcome');
+}
+newFeature()
